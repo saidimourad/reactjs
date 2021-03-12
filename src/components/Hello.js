e8db27db697748db2dbb19d37ee416bf0d1f3002
Saidi Mourad <saidimourad85@gmail.com>
2021-03-12 22:02:18 +0100

    created project

---
+++ src/components/Hello.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return ( <div> <h1> Heloo wrld </h1></div > );
+
+    }
+}
+export default Hello;
